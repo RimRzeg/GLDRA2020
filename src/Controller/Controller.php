9e6eab641f6ee61feb0f0b5142b49b2730525ffc
rim <rim.rzigue@gmail.com>
2020-03-05 12:46:41 +0100

    commit 1 of my project

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Response;
 class Controller extends AbstractController
 {
     /**
@@ -15,5 +15,35 @@ class Controller extends AbstractController
         return $this->render('/index.html.twig', [
             'controller_name' => 'Controller',
         ]);
+    }
+     /**
+     * @Route("/test", name="test")
+     */
+    public function index2()
+    {
+        return new Response("Bonjouur");
+       
+    }
+     /**
+     * @Route("/test3", name="test3")
+     */
+    public function index3()
+    {
+        $tab=['rim','jiji'];
+        return $this->render('/index3.html.twig', [
+            'names' => $tab,
+        ]);
+       
+    }
+     /**
+     * @Route("/somme/{n1}/{n2}", name="somme")
+     */
+    public function somme($n1,$n2):Response
+    {
+        $somme=$n1+$n2;
+        return $this->render('/somme.html.twig', [
+            'n1' => $n1,'n2'=>$n2,'somme'=>$somme
+        ]);
+       
     }
 }
